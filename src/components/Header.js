469f7d98354a514f6c4b3588570608d343789edc
Matheus Luís <suetham_luis@hotmail.com>
2018-09-26 23:28:25 -0300

    Creating first component

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <nav>
+        <div class="nav-wrapper">
+          <a href="/" class="left brand-logo">Emaily</a>
+          <ul id="nav-mobile" class="right">
+            <li><a href="">Log With Google </a></li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Header;
