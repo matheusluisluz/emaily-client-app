7e54698fe2a2da42eec1b808b6a33b794b761a7a
Matheus Luís <suetham_luis@hotmail.com>
2018-09-30 16:46:34 -0300

    Add stripe react component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 
 class Header extends Component {
@@ -14,7 +15,7 @@ class Header extends Component {
           <li><a href="/auth/google"> Login With Google </a></li>
         );
       default:
-        return <li><a href="/api/logout">Logout</a></li>
+        return <Payments><a href="/api/logout">Logout</a></Payments>
     }
   };
 
