b4092540372e17dedc4f71aae02fcee73f9f3c25
Matheus Luís <suetham_luis@hotmail.com>
2018-09-30 00:49:49 -0300

    Landing component and Link tags

---
+++ src/components/Landing.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class Landing extends Component {
+  render() {
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <h1>Emaily!</h1>
+        Collect feedback form your Users
+      </div>
+    )
+  }
+}
+
+export default Landing;
