d9ce5c7cc8c24587087044270dcb81395004a3c1
Matheus Luís <suetham_luis@hotmail.com>
2018-10-15 22:15:10 -0300

    Validation errors

--- src/components/surveys/SurveyForm.js
+++ src/components/surveys/SurveyForm.js
@@ -2,6 +2,7 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
 
 const FIELDS = [
@@ -24,11 +25,32 @@ class SurveyForm extends Component {
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
           {this.renderFields()}
-          <button className="btn" type="submit">Submit</button>
+          <Link to="/surveys" className="red btn-flat white-text">
+            Cancel
+          </Link>
+          <button className="teal btn-flat right white-text" type="submit">
+            Next
+            <i className="material-icons right">done</i>
+          </button>
         </form>
       </div>
     );
   }
 }
 
-export default reduxForm({ form: 'surveyForm' })(SurveyForm);
+function validate(values) {
+  const errors = {};
+
+  _.each(FIELDS, ({ name }) => {
+    if(!values[name]){
+      errors[name] = 'You must provide a value';
+    }
+  });
+
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'surveyForm'
+})(SurveyForm);
