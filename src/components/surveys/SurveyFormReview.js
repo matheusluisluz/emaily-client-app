6165ad8dfe996a10ec4f48880c0009a75bf6f198
Matheus Luís <suetham_luis@hotmail.com>
2018-10-22 22:32:46 -0300

    Advancing From SurveyForm

---
+++ src/components/surveys/SurveyFormReview.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class SurveyFormReview extends Component {
+  render() {
+    return (
+      <div>
+        <h5>Please confirm your entries</h5>
+      </div>
+    )
+  }
+}
